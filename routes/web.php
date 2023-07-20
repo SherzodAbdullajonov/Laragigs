4f9e30013b3aa45a117dc048564374d0faa55f34
SHERZOD ABDULLAJONOV <sabdullajonov77@gmail.com>
2023-07-20 15:42:03 +0500

    added components and partials folders

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,17 @@ use App\Models\Listing;
 
 Route::get('/', function () {
     return view('listings', [
-        'heading' => 'Latest Listings',
         'listings' => Listing::all() 
     ]);
 });
 
-Route::get('/listings/{id}', function($id){
-    $listing = Listing::find($id);
-    if($listing) {
+Route::get('/listings/{listing}', function(Listing $listing){
+  
+   
     return view('listing',[
-        'listing' => Listing::find($id)
+        'listing' => $listing
     ]);
-} else {
-    abort('404');
-}
+
 });
 
 
