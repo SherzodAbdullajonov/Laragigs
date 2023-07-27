6f32bdf2518795aa3a92f2be0b25076764487d44
SHERZOD ABDULLAJONOV <sabdullajonov77@gmail.com>
2023-07-27 14:00:24 +0500

    Create listing added

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
     //Show all listing
     public function index() {
         return view('listings.index', [
-            'listing' => Listing::latest()->filter(request(['tag']))->get()
+            'listing' => Listing::latest()->filter(request(['tag', 'search']))->get()
         ]);
     }
 
@@ -20,4 +21,25 @@ class ListingController extends Controller
             'listing' => $listing
         ]);
     }
+    //Show create form
+    public function create() {
+        return view('listings.create');
+    }
+
+    //Store Lising Data
+    public function store(Request $request)  {
+        $formField = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+        Listing::create($formField);
+
+        return redirect('/')->with('message', 'Listing created successfully!');
+    }
+
 }
