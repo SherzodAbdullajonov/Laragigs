6a37d386ffd9a478888f6f78b91a9d027e4e0800
SHERZOD ABDULLAJONOV <sabdullajonov77@gmail.com>
2023-07-28 00:51:07 +0500

    Update Listing added

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -8,25 +8,25 @@ use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
-    //Show all listing
+    //Show All Listing
     public function index() {
         return view('listings.index', [
             'listing' => Listing::latest()->filter(request(['tag', 'search']))->paginate(4)
         ]);
     }
 
-    //Show single listing
+    //Show Single Listing
     public function show(Listing $listing) {
         return view('listings.show', [
             'listing' => $listing
         ]);
     }
-    //Show create form
+    //Show Create Form
     public function create() {
         return view('listings.create');
     }
 
-    //Store Lising Data
+    //Store Listing Data
     public function store(Request $request) {
         $formFields = $request->validate([
             'title' => 'required',
@@ -47,6 +47,28 @@ class ListingController extends Controller
         Listing::create($formFields);
 
         return redirect('/')->with('message', 'Listing created successfully!');
+    }
+    //Update Listing Data
+    public function update(Request $request, Listing $listing) {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required'],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        if($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $formFields['user_id'] = auth()->id();
+
+        $listing->update($formFields);
+
+        return back()->with('message', 'Listing updated successfully!');
     }
 
     // Show Edit Form
